3b58032700eff307acf17fe13e83aa462cb849ff
FatGuyy <omkarjadhav2103@gmail.com>
2022-07-07 22:27:32 +0530

    Initializing the required files

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import os
-import constants
-from selenium import webdriver
+#Using api to fetch the data.
 
-os.environ['PATH'] += r'.\chromedriver.chromedriver.exe'
-driver = webdriver.Chrome()
+import os
+import requests
+import constants
+from pprint import pprint as pp
+
+#from selenium import webdriver
+#os.environ['PATH'] += r'.\chromedriver.chromedriver.exe'
+#driver = webdriver.Chrome()
+
+#get the request and fetch the data
+response = requests.get('')
+
+#Work with the data and get volume in a list
+
+
+#use the matplotlib and display the graph.
 
